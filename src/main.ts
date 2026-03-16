5b455d345f49e3ae49de6b1d75dc5ea257d3ec3f
Julián Isaza C <89994334+Julianisazac@users.noreply.github.com>
2026-03-15 20:03:24 -0500

    fix in the main files for use the cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
